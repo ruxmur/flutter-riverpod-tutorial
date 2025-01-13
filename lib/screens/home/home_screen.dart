3c78ef317a6227500c70ba1177e0aa9f5046bfc9
rux_mur <murtacaevaruxanda@bk.ru>
2025-01-13 11:38:04 +0200

    Lesson 6.

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -39,12 +39,12 @@ class HomeScreen extends ConsumerWidget {
 
                   if (cartProducts.contains(allProducts[index]))
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {ref.read(cartNotifierProvider.notifier).removeProduct(allProducts[index]);},
                       child: const Text('Remove'),
                     ),
                   if (!cartProducts.contains(allProducts[index]))
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {ref.read(cartNotifierProvider.notifier).addProduct(allProducts[index]);},
                       child: const Text('Add to cart'),
                     ),
                 ]
