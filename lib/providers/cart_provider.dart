1d0c21be36524e466149c76b53c922cc0739f988
rux_mur <murtacaevaruxanda@bk.ru>
2025-01-13 11:44:41 +0200

    Lesson 7.

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,13 +1,14 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:flutter_riverpod_tutorial/models/product.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
   // initial value
   @override
   Set<Product> build() {
-    return const{
-      Product(id: '1', title: 'Groovy Shorts', price: 12, image: 'assets/shorts.png'),
-    };
+    return const {};
   }
 
   // methods to update state
@@ -22,5 +23,3 @@ class CartNotifier extends Notifier<Set<Product>> {
     }
   }
 }
-
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() => CartNotifier());
